71f833c241630b097c171ff899cc04b026eb8f6d
paul brunache <paulbrunache@gmail.com>
2015-07-03 23:58:54 -0400

    adding flags

--- euphoria.gemspec
+++ euphoria.gemspec
@@ -25,6 +25,7 @@ Gem::Specification.new do |spec|
   spec.add_runtime_dependency "sass-rails", "~> 5.0.1"
   spec.add_runtime_dependency "font-awesome-rails", "~> 4.3.0.0"
   spec.add_runtime_dependency "coffee-rails", "~> 4.1.0"
+  spec.add_runtime_dependency "sass-rails", "~> 5.0"
     
   spec.add_development_dependency "bundler", "~> 1.10"
   spec.add_development_dependency "rake", "~> 10.0"
--- lib/generators/euphoria/install_generator.rb
+++ lib/generators/euphoria/install_generator.rb
@@ -1,30 +1,50 @@
 require 'rails/generators'
 
-
-
 module Euphoria
   class InstallGenerator < Rails::Generators::Base
+    desc "This generator creates all source files used for this framework"
     source_root File.expand_path('..', __FILE__)
-
-    # get stylesheets
-    def get_stylesheets
-      directory 'templates/stylesheets/', 'app/assets/stylesheets/'
-    end
-    def insert_css_and_js
+    
+    option :stylesheet, type: boolean, default: false
+    option :js, type: boolean, default: false
+    option :all, type: boolean, default: false
+    def install
+      if(option[:stylesheet])
+        copy_styles
+      elsif (option[:js])
+        copy_js
+      elsif (option[:all])
+        install_all
+      else
+        inject_css_and_js
+      end
+    end #end install method
+    private
+      def copy_js
+        directory 'templates/stylesheets/', 'app/assets/stylesheets/'
+      end #end copy JS
+      def copy_styles
+        directory 'templates/javascripts/', 'app/assets/javascripts/'
+      end #end copy styles
+    
+      def inject_css_and_js
         application_js_path = 'app/assets/javascripts/application.js'
         application_css_path ='app/assets/stylesheets/application.css'
-      
-        if ::File.exists?(::File.join(destination_root, application_js_path))
-          inject_into_file application_js_path, before: '//= require_tree' do
-            "//= require UI_Modules\n"
-          end
-        end
-        if ::File.exists?(::File.join(destination_root, application_css_path))
-            inject_into_file application_css_path, before: '//= require_tree' do
-              "//= require load\n"
+          if ::File.exists?(::File.join(destination_root, application_js_path))
+            inject_into_file application_js_path, before: '//= require_tree' do
+              "//= require UI_Modules\n"
             end
-        end
-    end
-  end
-end
-  
+          end
+          if ::File.exists?(::File.join(destination_root, application_css_path))
+              inject_into_file application_css_path, before: '//= require_tree' do
+                "*= require load\n"
+              end
+          end
+      end #end inject Css
+      def install_all
+        copy_styles
+        copy_js
+        insert_css_and_js
+      end
+  end #end Class Install Generator
+end #end Module
